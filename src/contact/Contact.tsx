5bf5fcb59cee6377376b691655677d71f1f80c3b
Wicher <wicher.m@gmail.com>
2023-03-11 23:59:31 +0100

    contact updated

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -3,9 +3,9 @@ import * as S from "./Contact.styles";
 export function Contact() {
   return (
     <S.Container id="contact">
-      <h3>email@email.com</h3>
-      <h3>+12 345 678 900</h3>
-      <h3>adres</h3>
+      <h3>wicher.m@gmail.com</h3>
+      <h3>+48 509 366 268</h3>
+      <h3>Katowice, POLAND</h3>
     </S.Container>
   );
 }
