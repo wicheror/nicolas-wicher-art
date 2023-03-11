28644aada53d6cc5e168786914e485e6d8dab554
Wicher <wicher.m@gmail.com>
2023-03-11 23:49:54 +0100

    image links all

--- src/gallery/Gallery.tsx
+++ src/gallery/Gallery.tsx
@@ -9,7 +9,7 @@ const photos = [
   { src: "https://user-images.githubusercontent.com/84172776/224514831-924750b9-3e1a-43d0-b94b-dcf942bf6ede.png", width: 1160, height: 1200 },
   { src: "https://user-images.githubusercontent.com/84172776/224514834-d2987682-a617-47b3-99ac-635866c35805.png", width: 885, height: 1182 },
   { src: "https://user-images.githubusercontent.com/84172776/224514835-70cec2fa-c821-4688-ba43-595a875af1e3.png", width: 1200, height: 1226 },
-  { src: "https://user-images.githubusercontent.com/84172776/224514836-e239bcb9-2a9a-46d2-a338-e07315c9d833.pn", width: 912, height: 1200 },
+  { src: "https://user-images.githubusercontent.com/84172776/224514836-e239bcb9-2a9a-46d2-a338-e07315c9d833.png", width: 912, height: 1200 },
 ];
 
 export function Gallery() {
