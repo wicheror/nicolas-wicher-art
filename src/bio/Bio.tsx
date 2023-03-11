800239a4f961cc2e8eb1a0f68eafde1b8704091c
Wicher <wicher.m@gmail.com>
2023-03-11 23:58:37 +0100

    bio updated

--- src/bio/Bio.tsx
+++ src/bio/Bio.tsx
@@ -3,8 +3,14 @@ import * as S from "./Bio.styles";
 export function Bio() {
   return (
     <S.Container id="bio">
-      <p>Akapit tekstu o mnie. I drugi tez</p>
-      <p>Inny akapit tekstu.</p>
+      <p>Meet Nicolas Wicher, a contemporary artist from Silesia (Poland) who believes that everything that creates an impression is art.</p>
+
+       <p>With a diverse range of skills and a love for cooperative projects, Nicolas has been creating thought-provoking works since 2014 as a game designer and entrepreneur.</p>
+       
+       <p>Passionate about creating impactful art, Nicolas uses a variety of mediums to bring out the essence of his abstract works, which challenge conventional concepts and showcase the boundless potential of creativity.</p>
+       
+       <p>Through his website, Nicolas invites you to join him on a journey of discovery and wonder, as you explore his private collection of abstract artworks.</p>
+   
     </S.Container>
   );
 }
