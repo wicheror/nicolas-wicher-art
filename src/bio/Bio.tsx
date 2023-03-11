86d1479d6c794fe7c26105a01528ee62c9b8facc
Wicher <wicher.m@gmail.com>
2023-03-11 23:32:19 +0100

    changes to bio

--- src/bio/Bio.tsx
+++ src/bio/Bio.tsx
@@ -3,7 +3,7 @@ import * as S from "./Bio.styles";
 export function Bio() {
   return (
     <S.Container id="bio">
-      <p>Akapit tekstu o mnie.</p>
+      <p>Akapit tekstu o mnie. I drugi tez</p>
       <p>Inny akapit tekstu.</p>
     </S.Container>
   );
