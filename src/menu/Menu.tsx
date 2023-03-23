20956efec627830cf84d62ca526def68b68f781e
Wicher <wicher.m@gmail.com>
2023-03-23 22:34:11 +0100

    menu item added

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -4,6 +4,9 @@ export function Menu() {
   return (
     <S.Container>
       <S.MenuList>
+      <S.MenuItem>
+          <S.Link href="https://shop.nicolaswicher.com">shop</S.Link>
+        </S.MenuItem>
         <S.MenuItem>
           <S.Link href="#gallery">gallery</S.Link>
         </S.MenuItem>
